c4babf10bf85282fa684cc21c2d03bef994ff759
BrandPM18 <bpalaciosm@uni.pe>
2021-05-05 23:55:31 -0500

    Edit pirate_crews.py fix all

--- scrapper/pirate_crews.py
+++ scrapper/pirate_crews.py
@@ -9,49 +9,53 @@ MAIN_SHIPS = ['Nostra_Castello','Red_Force','Miss_Love_Duck','Big_Top','Going_Me
               'Tontatta_Pirates','Saber_of_Xebec','Numancia_Flamingo','Wind_Granma']
 ALL_SHIPS = []
 
-def get_pirate_crews():
-    for ship in MAIN_SHIPS:
-        response = requests.get(BASE_URL+ship)
-        html = BeautifulSoup(response.text, 'html.parser')
-        dataDic = {}
+def get_pirate_crews(ship):
+    """
+    ship: end about the core, BASE_URL+ship = Core Url
+    :rtype: dict
+    """
+    response = requests.get(BASE_URL+ship)
+    html = BeautifulSoup(response.text, 'html.parser')
+    dataDic = {}
 
-        value = html.find("section", class_="pi-item pi-group pi-border-color")
-        value2 = html.find_all("section", class_="pi-item pi-group pi-border-color")
+    value = html.find("section", class_="pi-item pi-group pi-border-color")
+    value2 = html.find_all("section", class_="pi-item pi-group pi-border-color")
 
-        if len(value2)==2:
-            value = value2[1]
+    if len(value2)==2:
+        value = value2[1]
 
-        for data in zip(value.find_all("div", class_="pi-item pi-data pi-item-spacing pi-border-color"), value.find_all("div", class_="pi-data-value pi-font")):
-            data_source = data[0].attrs["data-source"]
-            dataDic[data_source] = data[1].text
+    for data in zip(value.find_all("div", class_="pi-item pi-data pi-item-spacing pi-border-color"), value.find_all("div", class_="pi-data-value pi-font")):
+        data_source = data[0].attrs["data-source"]
+        dataDic[data_source] = data[1].text
 
-        # print(dataDic)
+    # print(dataDic)
 
-        if 'jname' in dataDic.keys():
-            dataDic.pop('jname')
-        if 'N/A' in dataDic['ename']:
-            dataDic['ename'] = ship
+    if 'jname' in dataDic.keys():
+        dataDic.pop('jname')
+    if 'N/A' in dataDic['ename']:
+        dataDic['ename'] = ship
 
-        dataDic.setdefault('status','Unknown')
-        if 'ename' not in dataDic.keys():
-            if 'rname' in dataDic.keys():
-                dataDic.setdefault('ename',dataDic['rname'])
-            else:
-                dataDic.setdefault('ename','Unknown')
-        if 'first' not in dataDic.keys():
-            dataDic.setdefault('first','Unknown')
-        start = dataDic['first'].find('[')
-        end = dataDic['first'].rfind(']')
-
-        dataFirst = dataDic['first'].replace(dataDic['first'][start:end+1],'')
-        dataT =  dataFirst.split(', ') if (',' in dataFirst) else dataFirst.split('; ')
-
-        if 'Chapter' in dataT[0]:
-            dataDic['first'] = dataT[1]
+    dataDic.setdefault('status','Unknown')
+    if 'ename' not in dataDic.keys():
+        if 'rname' in dataDic.keys():
+            dataDic.setdefault('ename',dataDic['rname'])
         else:
-            dataDic['first'] = dataT[0]
+            dataDic.setdefault('ename','Unknown')
+    if 'first' not in dataDic.keys():
+        dataDic.setdefault('first','Unknown')
+    start = dataDic['first'].find('[')
+    end = dataDic['first'].rfind(']')
 
-        ALL_SHIPS.append(dataDic)
+    dataFirst = dataDic['first'].replace(dataDic['first'][start:end+1],'')
+    dataT =  dataFirst.split(', ') if (',' in dataFirst) else dataFirst.split('; ')
 
-    return ALL_SHIPS
+    if 'Chapter' in dataT[0]:
+        dataDic['first'] = dataT[1]
+    else:
+        dataDic['first'] = dataT[0]
+
+
+    #ALL_SHIPS.append(dataDic)
+
+    return dataDic
 
